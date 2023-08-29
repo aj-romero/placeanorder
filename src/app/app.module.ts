08939dcc85371c031b7eea12dd4ed260cfc7bbfb
Alber J Romero <alberjose.romero@gmail.com>
2023-08-29 16:06:41 -0600

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { MainComponent } from './componets/main/main.component';
+import { ProductComponent } from './componets/product/product.component';
+import { CartComponent } from './componets/cart/cart.component';
+import { StoreComponent } from './componets/store/store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    ProductComponent,
+    CartComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
