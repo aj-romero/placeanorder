8aacc90da78b589e5442079f6324e7e5005b586b
Alber J Romero <alberjose.romero@gmail.com>
2023-08-29 16:49:33 -0600

    adding the first endpoint

--- src/app/componets/main/main.component.ts
+++ src/app/componets/main/main.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product.interface';
+import { StoreService } from 'src/app/service/store.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit{
+  products:Product[]=[];
+  constructor(private storeService:StoreService){}
 
+  ngOnInit(): void{
+    this.storeService.getProducts().subscribe((res)=>{
+        this.products = res;
+        console.log(this.products);
+        
+    });
+  }
 }
--- src/app/service/store.service.ts
+++ src/app/service/store.service.ts
@@ -1,10 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../interfaces/product.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
+  api = 'http://localhost:9000/api/v1/';
+
   constructor(private httpClient: HttpClient) { }
+
+  getProducts(): Observable<Product[]>{
+    return this.httpClient.get<Product[]>(this.api + 'product/list');
+  }
 }
